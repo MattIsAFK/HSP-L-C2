dc253f9095d7dff917e670b438b097043b93e50f
MattIsAFK <56160359+MattIsAFK@users.noreply.github.com>
2019-10-28 23:08:32 +0100

    Add files via upload

--- Profil-Rechner_Console/Profil-Rechner_Console/Program.cs
+++ Profil-Rechner_Console/Profil-Rechner_Console/Program.cs
@@ -10,6 +10,54 @@ namespace Profil_Rechner_Console
     {
         static void Main(string[] args)
         {
+
+            double zBreite;
+            double zLänge;
+            double zHöhe;
+
+            // Eingabe
+            Console.WriteLine("Bitte geben Sie die Werte in Meter für Ihr Rechteckprofil ein.");
+            Console.Write("Breite:");
+            zBreite = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Länge:");
+            zLänge = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Höhe:");
+            zHöhe = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+
+            // Ausgabe
+
+            Console.WriteLine("Ergebnisse");
+            Console.ReadLine();
+            Console.WriteLine("Fläche:" + FlächenRechner(zBreite, zLänge)+" m²");
+            Console.WriteLine("Volumen:" + VolumenRechner(zBreite, zLänge, zHöhe) +" m³");
+            Console.WriteLine("Oberfläche:" + OberflächenRechner(zBreite, zLänge, zHöhe)+ " m²");
+            Console.WriteLine("Gewicht:" + GewichtRechner(zBreite, zLänge, zHöhe, 7900) + " kg");
+            Console.ReadLine();
+        }
+        // Fläche errechnen lassen
+        static double FlächenRechner (double zBreite, double zLänge)
+        {
+            double eFläche;
+            return eFläche = zBreite * zLänge;
+        }
+        // volumen berechnen
+        static double VolumenRechner(double zBreite, double zLänge, double zHöhe)
+        {
+            double eVolumen;
+            return eVolumen = zBreite * zLänge * zHöhe ;
+        }
+        //Gewicht berechnen
+        static double GewichtRechner(double zBreite, double zLänge, double zHöhe, double zDichte)
+        {
+            double eGewicht;
+            return eGewicht = zBreite * zLänge * zHöhe * zDichte ;
+        }
+        // Oberfläche berechnen lassen
+        static double OberflächenRechner(double zBreite, double zLänge, double zHöhe)
+        {
+            double eOber;
+            return eOber = 2*(zBreite * zLänge + zLänge * zHöhe + zBreite * zHöhe) ;
         }
     }
 }
