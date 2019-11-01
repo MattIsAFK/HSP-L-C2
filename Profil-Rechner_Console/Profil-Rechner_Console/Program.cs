860ff1a9dfa04b5bcf8edb9ee827bd7c4654d8a9
MattIsAFK <56160359+MattIsAFK@users.noreply.github.com>
2019-11-01 10:58:22 +0100

    Add files via upload

--- Profil-Rechner_Console/Profil-Rechner_Console/Program.cs
+++ Profil-Rechner_Console/Profil-Rechner_Console/Program.cs
@@ -10,54 +10,131 @@ namespace Profil_Rechner_Console
     {
         static void Main(string[] args)
         {
+            // Auswahl Variabeln
+            int AuswahlProfil;
 
+            /// Variabeln für Vierkantprofil
             double zBreite;
             double zLänge;
             double zHöhe;
+            /// Variabeln für Kreisprofil
+            double kRadius;
+            double kLänge;
 
-            // Eingabe
-            Console.WriteLine("Bitte geben Sie die Werte in Meter für Ihr Rechteckprofil ein.");
-            Console.Write("Breite:");
-            zBreite = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Welches Profil soll berechnet werden?");
+            Console.WriteLine("Für Vierkantprofil geben Sie bitte 1 ein");
+            Console.WriteLine("Für Kreisprofil geben Sie bitte 2 ein");
+            AuswahlProfil =Convert.ToInt32 (Console.ReadLine());
+
+
+            if (AuswahlProfil <= 1)
+            {
+                // Eingabe Vierkantprofil
+                Console.WriteLine("Bitte geben Sie die Werte in Meter für Ihr Rechteckprofil ein.");
+                Console.Write("Breite:");
+                zBreite = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Länge:");
+                zLänge = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Höhe:");
+                zHöhe = Convert.ToDouble(Console.ReadLine());
+                Console.ReadLine();
+
+                // Ausgabe Vierkantprofil
+
+                Console.WriteLine("Ergebnisse");
+                Console.ReadLine();
+                Console.WriteLine("Fläche:" + VierKantFlächenRechner(zBreite, zLänge) + " m²");
+                Console.WriteLine("Volumen:" + VierKantVolumenRechner(zBreite, zLänge, zHöhe) + " m³");
+                Console.WriteLine("Oberfläche:" + VierKantOberflächenRechner(zBreite, zLänge, zHöhe) + " m²");
+                Console.WriteLine("Gewicht:" + VierKantGewichtRechner(zBreite, zLänge, zHöhe, 7900) + " kg");
+                Console.ReadLine();
+
+            }
+            else _ = (AuswahlProfil >= 2);
+           { 
+            /// Eingabe Kreisprofil
+            /// 
+            Console.WriteLine("Bitte geben Sie die Werte in Meter für Ihr Kreisprofil ein.");
+            Console.Write("Radius:");
+            kRadius = Convert.ToDouble(Console.ReadLine());
             Console.Write("Länge:");
-            zLänge = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Höhe:");
-            zHöhe = Convert.ToDouble(Console.ReadLine());
+            kLänge = Convert.ToDouble(Console.ReadLine());
             Console.ReadLine();
 
-            // Ausgabe
+            ///Ausgabe Kreisprofil
 
             Console.WriteLine("Ergebnisse");
             Console.ReadLine();
-            Console.WriteLine("Fläche:" + FlächenRechner(zBreite, zLänge)+" m²");
-            Console.WriteLine("Volumen:" + VolumenRechner(zBreite, zLänge, zHöhe) +" m³");
-            Console.WriteLine("Oberfläche:" + OberflächenRechner(zBreite, zLänge, zHöhe)+ " m²");
-            Console.WriteLine("Gewicht:" + GewichtRechner(zBreite, zLänge, zHöhe, 7900) + " kg");
+            Console.WriteLine("Volumen:" + KreisVolumenRechner(kRadius, kLänge) + " m³");
+            Console.WriteLine("Oberfläche:" + KreisOberflächenRechner(kRadius, kLänge) + " m²");
+            Console.WriteLine("Mantelfläche:" + KreisMantelflächeRechner(kRadius, kLänge) + " m²");
+            Console.WriteLine("Grundfläche:" + KreisGrundflächeRechner(kRadius ) + " m²");
+            Console.WriteLine("Gewicht:" + KreisGewichtRechner( kRadius,  kLänge) + " kg");
             Console.ReadLine();
         }
-        // Fläche errechnen lassen
-        static double FlächenRechner (double zBreite, double zLänge)
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        //// Methoden
+        // Fläche errechnen lassen Vierkantprofil
+        static double VierKantFlächenRechner(double zBreite, double zLänge)
         {
             double eFläche;
             return eFläche = zBreite * zLänge;
         }
-        // volumen berechnen
-        static double VolumenRechner(double zBreite, double zLänge, double zHöhe)
+        // volumen berechnen Vierkantprofil
+        static double VierKantVolumenRechner(double zBreite, double zLänge, double zHöhe)
         {
             double eVolumen;
-            return eVolumen = zBreite * zLänge * zHöhe ;
+            return eVolumen = zBreite * zLänge * zHöhe;
         }
-        //Gewicht berechnen
-        static double GewichtRechner(double zBreite, double zLänge, double zHöhe, double zDichte)
+        //Gewicht berechnen Vierkantprofil
+        static double VierKantGewichtRechner(double zBreite, double zLänge, double zHöhe, double zDichte)
         {
             double eGewicht;
-            return eGewicht = zBreite * zLänge * zHöhe * zDichte ;
+            return eGewicht = zBreite * zLänge * zHöhe * zDichte;
         }
-        // Oberfläche berechnen lassen
-        static double OberflächenRechner(double zBreite, double zLänge, double zHöhe)
+        // Oberfläche berechnen lassen Vierkantprofil
+        static double VierKantOberflächenRechner(double zBreite, double zLänge, double zHöhe)
         {
             double eOber;
-            return eOber = 2*(zBreite * zLänge + zLänge * zHöhe + zBreite * zHöhe) ;
+            return eOber = 2 * (zBreite * zLänge + zLänge * zHöhe + zBreite * zHöhe);
+        }
+        // Volumen berechnen Kreisprofil
+        static double KreisVolumenRechner(double kRadius, double kLänge)
+        {
+            double eVolumen;
+            double Pi = 3.141592;
+            return eVolumen = Pi * (kRadius * kRadius) * kLänge;
+
+        }
+        // Oberfläche berechnen Kreiprofil
+        static double KreisOberflächenRechner(double kRadius, double kLänge)
+        {
+            double eOber;
+            double Pi = 3.141592;
+            return eOber = 2 * Pi * kRadius * (kRadius + kLänge);
+        }
+        // Mantelfläche berechnen Kreisprofil
+        static double KreisMantelflächeRechner(double kRadius, double kLänge)
+        {
+            double eMantel;
+            double Pi = 3.141592;
+            return eMantel = 2 * Pi * kRadius * kLänge;
+        }
+        // Grundfläche berechnen Kreisprofil
+        static double KreisGrundflächeRechner(double kRadius )
+        {
+            double eGrund;
+            double Pi = 3.141592;
+            return eGrund =  Pi * kRadius * kRadius;
+        }
+        //Gewicht berechnen Kreisprofil
+        static double KreisGewichtRechner(double kRadius, double kLänge)
+        {
+            double eGewicht;
+            double Pi = 3.141592;
+            return eGewicht = (Pi * (kRadius * kRadius) * kLänge) *7900;
         }
     }
 }
