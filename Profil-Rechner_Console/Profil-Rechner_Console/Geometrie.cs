8f9673ce70f1f48be103009b7ef4da6b946c93df
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-11-08 13:49:57 +0100

    Update Geometrie.cs

--- Profil-Rechner_Console/Profil-Rechner_Console/Geometrie.cs
+++ Profil-Rechner_Console/Profil-Rechner_Console/Geometrie.cs
@@ -9,8 +9,17 @@ namespace Profil_Rechner_Console
     abstract class Geometrie
     {
         protected double zLaenge;
+        /// <summary>
+        /// Erzeugt eine neue Geometrie der Länge 0.
+        /// Kann nur von "Kindern" aufgerufen werden da Klasse abstract.
+        /// </summary>
         public Geometrie():this(0) {}
 
+        /// <summary>
+        /// Erzeugt eine neue Geometrie von entsprechender Länge.
+        /// Wird nur von "Kindern" in deren Konstruktoren aufgerufen.
+        /// </summary>
+        /// <param name="pLaenge">Länge des zu erzeugenden Profils</param>
         public Geometrie(double pLaenge)
         {
             zLaenge = pLaenge;
