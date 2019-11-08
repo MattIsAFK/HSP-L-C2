e62dd519e0be6e2f8a944b340b029d7888db320f
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-11-08 12:03:52 +0100

    Update Rechteck.cs

--- Profil-Rechner_Console/Profil-Rechner_Console/Geometrien/Rechteck.cs
+++ Profil-Rechner_Console/Profil-Rechner_Console/Geometrien/Rechteck.cs
@@ -9,36 +9,56 @@ namespace Profil_Rechner_Console
     class Rechteck:Geometrie
     {
         /// Variabeln für Vierkantprofil
-        protected double breite;
-        protected double laenge;
-        protected double hoehe;
+        protected double zBreite;
+        protected double zLaenge;
+        protected double zHoehe;
 
 
         public Rechteck()
         {
-            breite = 0;
-            laenge = 0;
-            hoehe = 0;
+            zBreite = 0;
+            zLaenge = 0;
+            zHoehe = 0;
         }
         
         public override double GetFlaecheninhalt()
         {
-            return laenge*breite;
+            return zLaenge*zBreite;
+        }
+
+        public override double GetFlachenTraegheit()
+        {
+            throw new NotImplementedException();
         }
 
         public double getBreite()
         {
-            return breite;
+            return zBreite;
+        }
+
+        public void setBreite(double pBreite)
+        {
+            zBreite = pBreite;
         }
 
         public double getLaenge()
         {
-            return laenge;
+            return zLaenge;
+        }
+
+        public void setLaenge(double pLaenge)
+        {
+            zLaenge = pLaenge;
         }
 
         public double getHoehe()
         {
-            return hoehe;
+            return zHoehe;
+        }
+
+        public void setHoehe(double pHoehe)
+        {
+            zHoehe = pHoehe;
         }
     }
 
