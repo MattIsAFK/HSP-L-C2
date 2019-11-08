e6b88a460a1aa06b9a7c6d7f2b4006a14ed58114
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-11-08 11:40:19 +0100

    Create Geometrie.cs

---
+++ Profil-Rechner_Console/Profil-Rechner_Console/Geometrie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profil_Rechner_Console
+{
+    class Geometrie
+    {
+        public Geometrie()
+        {
+
+        }
+    }
+}
