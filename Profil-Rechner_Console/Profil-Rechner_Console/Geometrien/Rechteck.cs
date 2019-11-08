5b220bf292c6d1adf4edff26f1018faed3d973d6
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-11-08 11:56:39 +0100

    Update Rechteck.cs

--- Profil-Rechner_Console/Profil-Rechner_Console/Geometrien/Rechteck.cs
+++ Profil-Rechner_Console/Profil-Rechner_Console/Geometrien/Rechteck.cs
@@ -8,11 +8,38 @@ namespace Profil_Rechner_Console
 {
     class Rechteck:Geometrie
     {
+        /// Variabeln für Vierkantprofil
+        protected double breite;
+        protected double laenge;
+        protected double hoehe;
+
+
         public Rechteck()
         {
-            
+            breite = 0;
+            laenge = 0;
+            hoehe = 0;
         }
         
+        public override double GetFlaecheninhalt()
+        {
+            return laenge*breite;
+        }
+
+        public double getBreite()
+        {
+            return breite;
+        }
+
+        public double getLaenge()
+        {
+            return laenge;
+        }
+
+        public double getHoehe()
+        {
+            return hoehe;
+        }
     }
 
 }
