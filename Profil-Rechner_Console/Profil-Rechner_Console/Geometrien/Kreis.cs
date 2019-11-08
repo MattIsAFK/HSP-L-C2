e48cd2ce263d7b3e02bd96641c7cb0c31183996f
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-11-08 12:17:53 +0100

    Create Kreis.cs

---
+++ Profil-Rechner_Console/Profil-Rechner_Console/Geometrien/Kreis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profil_Rechner_Console.Geometrien
+{
+    class Kreis:Geometrie
+    {
+        public Kreis()
+        {
+
+        }
+
+        public override double GetFlaecheninhalt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetFlachenTraegheit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetVolumen()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
