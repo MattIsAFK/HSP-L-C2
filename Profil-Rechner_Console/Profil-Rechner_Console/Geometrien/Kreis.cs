ea468db92897089c3011630f832e7a9375247443
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-11-15 09:56:12 +0100

    Update Kreis.cs

--- Profil-Rechner_Console/Profil-Rechner_Console/Geometrien/Kreis.cs
+++ Profil-Rechner_Console/Profil-Rechner_Console/Geometrien/Kreis.cs
@@ -9,11 +9,20 @@ namespace Profil_Rechner_Console.Geometrien
     class Kreis:Geometrie
     {
         private double zRadius;
-
+        /// <summary>
+        /// Erzeugt einen neuen Kreis mit allen Feldwerten 0
+        /// </summary>
         public Kreis() : this(0, 0) { }
-
+        /// <summary>
+        /// Erzeugt ein Objekt Kreis mti übergebenem Radius und derProfillänge 0.
+        /// </summary>
+        /// <param name="pRadius">Radius des Kreises</param>
         public Kreis(double pRadius):this(pRadius,0) { }
-
+        /// <summary>
+        /// Erzeugt einen neuen Kreis mit den übergebenen Parametern.
+        /// </summary>
+        /// <param name="pRadius">Radius des Kreises</param>
+        /// <param name="pLaenge">Länge der Profilgeometrie</param>
         public Kreis(double pRadius, double pLaenge):base(pLaenge)
         {
             setRadius(pRadius);
