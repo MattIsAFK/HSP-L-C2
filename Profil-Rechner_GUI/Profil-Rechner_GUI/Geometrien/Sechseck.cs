2bce11bcc85e536abcec062e17b9023941b21151
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-11-27 16:47:47 +0100

    Create Sechseck.cs

---
+++ Profil-Rechner_GUI/Profil-Rechner_GUI/Geometrien/Sechseck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profil_Rechner_GUI.Geometrien
+{
+    class Sechseck:Geometrie 
+    {
+        public Sechseck():base()
+        {
+
+        }
+
+        public override double GetFlaecheninhalt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetFlachenTraegheit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetVolumen()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
