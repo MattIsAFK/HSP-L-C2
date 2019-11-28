16dc0c126e03b229bdb993305b05d583f1d6c866
Alexander-Schneider2k <Alexander-Schneider2k@users.noreply.github.com>
2019-11-28 11:58:57 +0100

    Update Sechseck.cs
    Methoden implementiert und Höhe rausgenommen, da nicht benötigt.

--- Profil-Rechner_GUI/Profil-Rechner_GUI/Geometrien/Sechseck.cs
+++ Profil-Rechner_GUI/Profil-Rechner_GUI/Geometrien/Sechseck.cs
@@ -12,32 +12,27 @@ namespace Profil_Rechner_GUI.Geometrien
         /// Variabeln für ein Sechseckprofil
 
         private double zBreite;
-        private double zHoehe;
-        private double zLaenge;
 
         /// Erzeugt ein Sechseck mit allen Variablen 0
 
-        public Sechseck() : this(0, 0, 0) { }
+        public Sechseck() : this(0, 0) { }
 
         /// <summary>
         /// Erzeugt ein neues Sechseck mit den vorherigen Maßen und der Laenge 0
         /// </summary>
         /// <param name="pBreite"></param>
-        /// <param name="pHoehe"></param>
 
-        public Sechseck(double pBreite, double pHoehe) : this(pBreite, pHoehe, 0) { }
+        public Sechseck(double pBreite) : this(pBreite, 0) { }
 
         /// <summary>
         /// Erzeugt ein neues Sechseck mit den jeweiligen Maßen
         /// </summary>
         /// <param name="pBreite"></param>
-        /// <param name="pHoehe"></param>
         /// <param name="pLaenge"></param>
 
-        public Sechseck(double pBreite, double pHoehe, double pLaenge) : base(pLaenge)
+        public Sechseck(double pBreite, double pLaenge) : base(pLaenge)
         {
             this.SetBreite(pBreite);
-            this.SetHoehe(pHoehe);
             this.SetLaenge(pLaenge);
         }
 
@@ -81,8 +76,29 @@ namespace Profil_Rechner_GUI.Geometrien
         }
 
 
+        /////////////////////////////////////////////////////////////////////////////
+        /// GET and SET
 
+        /// <summary>
+        /// Getter für das Feld Breite des Sechsecks bzw. der Sechseckseiten.
+        /// </summary>
+        /// <returns>Breite des Sechseckseiten</returns>
+        public double GetBreite()
+        {
+            return zBreite;
+        }
 
+        /// <summary>
+        /// Überprüft, dass der übergebene Parameter nicht negativ ist.
+        /// Im positiven Fall wird die Objektvariable entsprechend gesetzt, andernfalls eine Exception geworfen.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <param name="pBreite">Breite des Rechtecks</param>
+        public void SetBreite(double pBreite)
+        {
+            if (pBreite >= 0) zBreite = pBreite;
+            else throw new ArgumentOutOfRangeException("Breite muss einen positiven Wert annehmen");
+        }
 
     }
 }
