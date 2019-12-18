e920c55fa8166ca3359241c3d367bed3deae374b
Alexander-Schneider2k <Alexander-Schneider2k@users.noreply.github.com>
2019-12-18 11:04:49 +0100

    Create Kasten.cs
    Erstellung Klasse Kasten in Geometrie

---
+++ Profil-Rechner_GUI/Profil-Rechner_GUI/Geometrien/Kasten.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profil_Rechner_GUI.Geometrien
+{
+    class Kasten:Geometrie
+    {
+
+        /// Variabeln für ein Kastenprofil
+
+        private double zBreite;
+        private double zHoehe;
+
+        /// Erzeugt einen Kasten mit allen Variablen 0
+
+        public Kasten() : this(0, 0, 0) { }
+
+        /// <summary>
+        /// Erzeugt einen neuen Kasten mit den vorherigen Maßen und der Laenge 0
+        /// </summary>
+        /// <param name="pBreite"></param>
+        /// <param name="pHoehe"></param>
+
+        public Kasten(double pBreite, double pHoehe) : this(pBreite, pHoehe, 0) { }
+
+        /// <summary>
+        /// Erzeugt einen neuen Kasten mit den jeweiligen Maßen
+        /// </summary>
+        /// <param name="pBreite"></param>
+        /// <param name="pHoehe"></param>
+        /// <param name="pLaenge"></param>
+
+        public Kasten(double pBreite, double pHoehe, double pLaenge) : base(pLaenge)
+        {
+            this.SetBreite(pBreite);
+            this.SetHoehe(pHoehe);
+        }
+
+
+        /// <summary>
+        /// Berechnet den Flaecheninhalt des Kastens mit den global verfuegbaren Parametern der Klasse.
+        /// </summary>
+        /// <return>Flaecheninhalt des Kastenprofils</return>
+
+        public override double GetFlaecheninhalt()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Berechnet die Flaechentraegheit des Kastenprofils mit den global verfügbaren Parametern der Klasse.
+        /// </summary>
+        /// <return>Flaechentraegheit des Kastenprofils</return>
+
+        public override double[] GetFlaechenTraegheit()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Aus den gegebenen Paramnetern wird die Oberflaeche eines Kastenprofils berechnet.
+        /// </summary>
+        /// <returns>Oberflaeche</returns>
+        public double GetOberflaeche()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetBreite(double pBreite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetHoehe(double pHoehe)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
