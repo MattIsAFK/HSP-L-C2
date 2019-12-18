6da4a5e4dbfa415a5f65ed34b6964e4823e968c3
Alexander-Schneider2k <Alexander-Schneider2k@users.noreply.github.com>
2019-12-18 13:46:13 +0100

    Update Traeger.cs
    Erstellung Traeger

--- Profil-Rechner_GUI/Profil-Rechner_GUI/Geometrien/Traeger.cs
+++ Profil-Rechner_GUI/Profil-Rechner_GUI/Geometrien/Traeger.cs
@@ -15,7 +15,7 @@ namespace Profil_Rechner_GUI.Geometrien
 
         /// Erzeugt einen Traeger mit allen Variablen 0
 
-        public Traeger() : this(0, 0, 0) { }
+        public Traeger() : this(0, 0, 0, 0, 0) { }
 
         /// <summary>
         /// Erzeugt einen neuen Traeger mit den vorherigen Maßen und der Laenge 0
@@ -23,7 +23,7 @@ namespace Profil_Rechner_GUI.Geometrien
         /// <param name="pBreite"></param>
         /// <param name="pHoehe"></param>
 
-        public Traeger(double pBreite, double pHoehe) : this(pBreite, pHoehe, 0) { }
+        public Traeger(double pBreite1, double pHoehe1, double pBreite2, double pHoehe2) : this(pBreite1, pHoehe1, pBreite2, pHoehe2, 0) { }
 
         /// <summary>
         /// Erzeugt einen neuen Traeger mit den jeweiligen Maßen
@@ -31,11 +31,15 @@ namespace Profil_Rechner_GUI.Geometrien
         /// <param name="pBreite"></param>
         /// <param name="pHoehe"></param>
         /// <param name="pLaenge"></param>
+        /// <param name="pHoehe2"></param>
+        /// <param name="pLaenge"></param>
 
-        public Traeger(double pBreite, double pHoehe, double pLaenge) : base(pLaenge)
+        public Traeger(double pBreite1, double pHoehe1, double pBreite2, double pHoehe2, double pLaenge) : base(pLaenge)
         {
-            this.SetBreite(pBreite);
-            this.SetHoehe(pHoehe);
+            this.SetBreite1(pBreite1);
+            this.SetHoehe1(pHoehe1);
+            this.SetBreite2(pBreite2);
+            this.SetHoehe2(pHoehe2);
         }
 
         /// <summary>
@@ -59,6 +63,15 @@ namespace Profil_Rechner_GUI.Geometrien
         }
 
         /// <summary>
+        /// Aus den gegebenen Paramnetern wird die Mantelflaeche eines Traegerprofils berechnet.
+        /// </summary>
+        /// <returns>Oberflaeche</returns>
+        public double Mantelflaeche()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// Aus den gegebenen Paramnetern wird die Oberflaeche eines Traegerprofils berechnet.
         /// </summary>
         /// <returns>Oberflaeche</returns>
@@ -67,16 +80,25 @@ namespace Profil_Rechner_GUI.Geometrien
             throw new NotImplementedException();
         }
 
-        public void SetBreite(double pBreite)
+        public void SetBreite1(double pBreite1)
         {
             throw new NotImplementedException();
         }
 
-        public void SetHoehe(double pHoehe)
+        public void SetHoehe1(double pHoehe1)
         {
             throw new NotImplementedException();
         }
 
+        public void SetBreite2(double pBreite2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetHoehe2(double pHoehe2)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
