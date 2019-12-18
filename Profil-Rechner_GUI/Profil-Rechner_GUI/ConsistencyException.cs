15764b2d7e76b24b1d7db1b97ae0d0e014885160
MalShaunWho <56395808+MalShaunWho@users.noreply.github.com>
2019-12-18 01:10:49 +0100

    Erstellen der neuen Exception-Klasse ConsistencyException.cs
    Eine neue Exception für die Konsistenzprüfungen die gesondert abgefangen werden kann im Prüffall

---
+++ Profil-Rechner_GUI/Profil-Rechner_GUI/ConsistencyException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Profil_Rechner_GUI
+{
+    public class ConsistencyException : Exception
+    {
+        public ConsistencyException() : base() { }
+        public ConsistencyException(string text):base(text) { }
+    }
+}
